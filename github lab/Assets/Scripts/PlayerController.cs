186e11d3abbe0f38760e90cc9b747950c56b50c9
bobasharks <130084813+bobasharks@users.noreply.github.com>
2023-10-31 13:24:30 -0700

    moved objects and added move and jump function to playercontroller script

--- github lab/Assets/Scripts/PlayerController.cs
+++ github lab/Assets/Scripts/PlayerController.cs
@@ -5,5 +5,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //implement player movement
+    public float speed;
+    private Vector3 movePos;
 
+    private void MovePlayer()
+    {
+
+    }
+
+    private void Jump()
+    {
+        //makes the player jump
+    }
 }
